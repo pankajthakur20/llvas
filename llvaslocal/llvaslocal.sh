a17c3d8d64c092234778a65337a69428f51f187c
pankajthakur20 <41935273+pankajthakur20@users.noreply.github.com>
2020-05-16 23:07:00 +0530

    Update llvaslocal.sh

--- llvaslocal/llvaslocal.sh
+++ llvaslocal/llvaslocal.sh
@@ -3,10 +3,10 @@ date
 echo  -e "\nOpening Camera..."
 python3 llvaslocal.py
 echo -e "\nSending images to cloud \n"
-scp -i k30.pem ~/llvaslocal/attendance/*gray.jpg ubuntu@ec2-54-163-35-10.compute-1.amazonaws.com:/home/ubuntu/llvascloud/attendance/
+scp -i k30.pem ~/llvaslocal/attendance/*gray.jpg ubuntu@your-public-dns-ipv4:/home/ubuntu/llvascloud/attendance/
 echo -e "\nFinding faces in each image (in cloud)...\n"
-ssh -i k30.pem ubuntu@ec2-54-163-35-10.compute-1.amazonaws.com
+ssh -i k30.pem ubuntu@your-public-dns-ipv4
 echo -e "\nSending attendance graph back to laptop..."
-scp -i k30.pem ubuntu@ec2-54-163-35-10.compute-1.amazonaws.com:/home/ubuntu/llvascloud/attendance/* /home/pankaj/llvaslocal/attendance
+scp -i k30.pem ubuntu@your-public-dns-ipv4:/home/ubuntu/llvascloud/attendance/* /home/pankaj/llvaslocal/attendance
 date
 
